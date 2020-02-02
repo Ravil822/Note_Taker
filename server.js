d6cff1fcacb04472c48aa503e0e23f6462dd9bc0
RAvil <russian82@mail.com>
2020-02-01 23:38:56 -0500

    Start

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 4000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//==============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+ app.get("", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
