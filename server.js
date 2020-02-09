54a657e644ccca5684415ba9517295546642f02e
RAvil <russian82@mail.com>
2020-02-08 21:31:30 -0500

    added routes to add, and delete notes

--- server.js
+++ server.js
@@ -3,18 +3,25 @@
 const express = require("express");
 const path = require("path");
 
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 4000;
 
+
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//==============================================================
 
 // Basic HTML route 
+// =============================================================
+  app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
@@ -28,9 +35,51 @@ app.use(express.json());
   });
 
 
+  // Route display notes
+  // =============================================================
+  let dataDb = require("./db/db");
+  let id = 0;
+
+  app.get("/api/notes", function (req, res) {
+    res.json(dataDb);
+    // console.log(dataDb)
+  });
+
+
+  // Route to add notes
+  // =============================================================
+  app.post("/api/notes", function (req, res){
+    id++;
+    let newNote = {
+      title: req.body.title,
+      text: req.body.text
+    }
+    newNote.id = id;
+    dataDb.push(newNote);
+    // console.log(dataDb);
+    res.json(newNote);
+  });
+
+
+  // Route to delete notes
+  // =============================================================
+  app.delete("/api/notes/:id", function (req, res){
+    let targetId = req.params.id;
+    // console.log (targetId);
+    let delNote = dataDb.filter(note =>{ 
+      return note.id == targetId;
+  })[0];
+    const target = dataDb.indexOf(delNote);
+    // console.log(target);
+    dataDb.splice(target, targetId);
+    let newData = JSON.stringify(dataDb);
+    res.json(newData);
+  })
+
+
 
 // Listener
-
+// =============================================================
   app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
